bf7b95624ca8f44fc5e0e15982645d5cad45e24a
deca <idrisadejumo@gmail.com>
2023-03-12 18:54:41 +0100

    Getting A Single Video from the database

--- src/main/java/com/idris/Movies3/controller/MovieController.java
+++ src/main/java/com/idris/Movies3/controller/MovieController.java
@@ -2,14 +2,17 @@ package com.idris.Movies3.controller;
 
 import com.idris.Movies3.entity.Movie;
 import com.idris.Movies3.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,6 +22,11 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{Id}")
+    public  ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId Id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(Id), HttpStatus.OK );
     }
 
 }
