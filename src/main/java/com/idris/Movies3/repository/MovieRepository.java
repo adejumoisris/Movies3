bc9c0316e630ae357d4d74f43d18fcb2c5d8d227
deca <idrisadejumo@gmail.com>
2023-03-12 17:53:17 +0100

    creating A Repository Interface with helps to Create ORM relationship to the MongoDB database

---
+++ src/main/java/com/idris/Movies3/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.idris.Movies3.repository;
+
+import com.idris.Movies3.entity.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
